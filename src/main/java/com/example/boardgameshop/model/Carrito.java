f2881b459dbc052591dd0f366f87d6de6c24121b
Deuxpalta <gabrieleduardoveraf7@gmail.com>
2025-11-13 13:14:42 -0300

    second commit
    idk, i think it's done

--- src/main/java/com/example/boardgameshop/model/Carrito.java
+++ src/main/java/com/example/boardgameshop/model/Carrito.java
@@ -5,10 +5,9 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.AllArgsConstructor;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,23 +15,13 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-
-public class Producto {
-
+public class Carrito {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-    private String description;
-    private Double price;
-    private Integer stock;
+    
+    @OneToMany(mappedBy = "carrito")
+    private List<Producto> productos;
+    private Double total;
 
-    @ManyToOne
-    private Carrito carrito;
-
-    @OneToMany(mappedBy = "producto")
-    private List<Resenia> resenias;
-
-    @ManyToOne
-    private Categoria categoria;
 }
---
+++ src/main/java/com/example/boardgameshop/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package com.example.boardgameshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.boardgameshop.model.Usuario;
+import com.example.boardgameshop.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll() {
+        return usuarioRepository.findAll();
+    }
+
+    @SuppressWarnings("null")
+    public Usuario findById(Long id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    @SuppressWarnings("null")
+    public Usuario save(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void delete(Long id) {
+        usuarioRepository.deleteById(id);
+    }
+}
